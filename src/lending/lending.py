d6a817e764dbf7c3bbfd6a52ab5b69923cbfebb2
ThatCark <marc-heidecker@proton.me>
2026-08-02 17:20:43 +0200

    Add lending class

---
+++ src/lending/lending.py
@@ -0,0 +1,21 @@
+class Lending:
+    def __init__(self, lender, borrower, media):
+        self.__lender = lender
+        self.__borrower = borrower
+        self.__media = media
+        #lenddate?
+        #returndate?
+
+@property
+def lender(self):
+    return self.__lender
+
+@property
+def borrower(self):
+    return self.__borrower
+
+@property
+def media(self):
+    return self.__media
+
+    
